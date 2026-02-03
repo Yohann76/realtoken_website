2fbc7be247967967f16a0110990e962b8ae46554
Yohann76 <yohanndurand76@gmail.com>
2026-02-03 13:04:38 +0000

    feat: update wording in partner page

--- src/views/Partenaires.vue
+++ src/views/Partenaires.vue
@@ -50,6 +50,29 @@
             </span>
             <span class="offer-text">{{ $t('partenaires.offer3') }}</span>
           </li>
+          <li>
+            <span class="offer-icon" aria-hidden="true">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
+                <path d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4zm0 10.99h7c-.53 4.12-3.28 7.79-7 8.94V12H5V6.3l7-3.11v8.8z"/>
+              </svg>
+            </span>
+            <span class="offer-text">{{ $t('partenaires.offer4') }}</span>
+          </li>
+        </ul>
+      </div>
+
+      <div class="partenaires-block">
+        <h2 class="section-title">{{ $t('partenaires.seekTitle') }}</h2>
+        <p class="section-desc">{{ $t('partenaires.seekIntro') }}</p>
+        <ul class="offer-list seek-list">
+          <li v-for="(item, i) in seekItems" :key="i">
+            <span class="offer-icon" aria-hidden="true">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
+                <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
+              </svg>
+            </span>
+            <span class="offer-text">{{ item }}</span>
+          </li>
         </ul>
       </div>
 
@@ -64,6 +87,14 @@
 </template>
 
 <script setup>
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+
+const seekItems = computed(() =>
+  t('partenaires.seekList').split('\n').filter(Boolean)
+)
 </script>
 
 <style scoped>
